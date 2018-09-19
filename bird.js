2c0dd48451dd23d6e7a89f0167ef74ff04a80dc6
nguyenducvinh09 <nguyenducvinh09@gmail.com>
2018-09-19 10:52:04 +0700

    add sayHi method for bird.js

--- bird.js
+++ bird.js
@@ -1,7 +1,13 @@
-function Bird() {
+var chalk = require('chalk');
+function Bird(name) {
+	this.name = name;
 	this.stomach = [];
+}
+Bird.prototype.sayHi = function() {
+	console.log('Hi ! My name is Bird: ' + chalk.blue(this.name))
 }
 Bird.prototype.eat = function(cat) {
 	//body
 	this.stomach.push(cat);
 };
+module.exports = Bird;
---
+++ index.js
@@ -0,0 +1,3 @@
+var Bird = require('./bird');
+var bird1 = new Bird('penguins');
+bird1.sayHi();
